e039513a7a6a906efb282f68843db7c45528b3bc
TomAigner <tomaigner@gmail.com>
2023-06-12 10:07:35 -0400

    Update SignedIn.py

--- Tests/MainPage/SignedIn.py
+++ Tests/MainPage/SignedIn.py
@@ -1,6 +1,7 @@
 from playwright.sync_api import sync_playwright, expect
 from Pages.MainPage.MainPage import MainPage
 from Requests.Analytics.GoogleAnalytics import GoogleAnalytics
+from Requests.Analytics.Segment import Segment
 
 main_page = MainPage()
 
@@ -9,3 +10,4 @@ def test_sign_in(username, password):
     page = main_page.sign_in(username, password)
     expect(page).to_have_url('https://www.chewy.com/app/account')
     expect(len(GoogleAnalytics.requests_list)).not_to_have_count(0)
+    expect(len(Segment.requests_list)).not_to_have_count(0)
